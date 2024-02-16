436b5aee5883030f200651ceeada7176a968548b
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-02-16 05:36:11 +0100

    serving static files

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+});
+
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
